d37604dcac33d5d596798b5b1c22db9319a7ccc4
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-17 10:31:17 +1000

    Config + lang

--- lang/de.py
+++ lang/de.py
@@ -1,1 +1,7 @@
-
+RUNNING = "Server gestartet“ 
+MOTD1 = „"PieMC-Server“ 
+MOTD2 = "PYTHON“ 
+NEWPACKET = "Neues GamePacket von“
+DISCONNECTED = "Getrennt.“
+CONNECTING = "Verbinden…“ 
+DISCONNECT_PACKET = "PieMC-Bedrock ist noch nicht fertig.\nWir arbeiten Tag und Nacht daran.\nWenn Sie uns helfen möchten, können Sie einen Beitrag leisten unter:\nhttps://github.com/LapisMYT/PieMC_Bedrock“
