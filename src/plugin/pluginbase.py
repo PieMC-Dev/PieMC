bafc28984f20dca1f78c321e0a29ff87b1113334
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-17 10:24:22 +1000

    Delete src/plugin directory

--- src/plugin/pluginbase.py
+++
@@ -1,9 +0,0 @@
-class PluginBase():
-  construct __init__():
-    Values = {
-"Version": plugin.ver,
-"API": plugin.api,
-"Author": plugin.auth,
-"Permission": plugin.perm,
-"Name": plugin.name,
-    }
