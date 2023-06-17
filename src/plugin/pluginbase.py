1577762e6a62c3dc62b7a8fd915389071d2c0c4b
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-17 10:21:29 +1000

    Create pluginbase.py

---
+++ src/plugin/pluginbase.py
@@ -0,0 +1,9 @@
+class PluginBase():
+  construct __init__():
+    Values = {
+"Version": plugin.ver,
+"API": plugin.api,
+"Author": plugin.auth,
+"Permission": plugin.perm,
+"Name": plugin.name,
+    }
