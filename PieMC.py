7c9c3e5986c7f73c76e6392525c5307b1bfc661a
andiricum2 <andiricum2@gmail.com>
2023-06-23 16:25:51 +0200

    Bye Problems

---
+++ PieMC.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.call(["python", "__main__.py"])
--- server.py
+++ server.py
@@ -10,7 +10,7 @@ from packets.offline_ping import OfflinePing
 lang_dirname = "lang"
 file_to_find = config.LANG + ".py"
 
-lang_fullpath = os.path.join(os.getcwd(), 'PieMC', lang_dirname)
+lang_fullpath = os.path.join(os.getcwd(), lang_dirname)
 
 if os.path.exists(lang_fullpath):
     lang_path = os.path.join(lang_fullpath, file_to_find)
