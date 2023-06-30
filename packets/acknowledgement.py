e691cd197ecce0490bce3f6b841f5bcb842af105
LapisMYT <nikitagavrilin005@gmail.com>
2023-06-30 12:50:47 +0300

    Fixed Acknowledgement Update acknowledgement.py Update ack.py Update nack.py

--- packets/acknowledgement.py
+++ packets/acknowledgement.py
@@ -1,6 +1,7 @@
 from packets.packet import Packet
 from buffer import Buffer
 
+
 class Acknowledgement(Packet):
     def __init__(self, data: bytes = b"", pos: int = 0):
         super().__init__(data, pos)
@@ -8,18 +9,18 @@ class Acknowledgement(Packet):
 
     def decode_payload(self) -> None:
         self.sequence_numbers.clear()
-        count: int = self.read_unsigned_short_be()
+        count: int = self.read_short()
         for i in range(0, count):
             single: bool = self.read_bool()
             if not single:
-                index: int = self.read_unsigned_triad_le()
-                end_index: int = self.read_unsigned_triad_le()
+                index: int = self.read_uint24le()
+                end_index: int = self.read_uint24le()
                 while index <= end_index:
                     self.sequence_numbers.append(index)
                     index += 1
             else:
-                self.sequence_numbers.append(self.read_unsigned_triad_le())
-        
+                self.sequence_numbers.append(self.read_uint24le())
+
     def encode_payload(self) -> None:
         self.sequence_numbers.sort()
         temp_buffer: Buffer = Buffer()
@@ -35,21 +36,21 @@ class Acknowledgement(Packet):
                 elif diff > 1:
                     if start_index == end_index:
                         temp_buffer.write_bool(True)
-                        temp_buffer.write_unsigned_triad_le(start_index)
+                        temp_buffer.write_uint24le(start_index)
                         start_index = end_index = current_index
                     else:
                         temp_buffer.write_bool(False)
-                        temp_buffer.write_unsigned_triad_le(start_index)
-                        temp_buffer.write_unsigned_triad_le(end_index)
+                        temp_buffer.write_uint24le(start_index)
+                        temp_buffer.write_uint24le(end_index)
                         start_index = end_index = current_index
                     count += 1
             if start_index == end_index:
                 temp_buffer.write_bool(True)
-                temp_buffer.write_unsigned_triad_le(start_index)
+                temp_buffer.write_uint24le(start_index)
             else:
                 temp_buffer.write_bool(False)
-                temp_buffer.write_unsigned_triad_le(start_index)
-                temp_buffer.write_unsigned_triad_le(end_index)
+                temp_buffer.write_uint24le(start_index)
+                temp_buffer.write_uint24le(end_index)
             count += 1
-            self.write_unsigned_short_be(count)
+            self.write_short(count)
             self.write(temp_buffer.data)
--- packets/nack.py
+++ packets/nack.py
@@ -1,23 +1,6 @@
 from packets.acknowledgement import Acknowledgement
 from ProtocolInfo import ProtocolInfo
-from packets.packet import Packet
 
-class Nack(Packet):
-    
-    packet_id = 0xa0
-    record_count: int = None
-    record_single_sequence_number: bool = None
-    record_start_sequence_number: bytes = None
-    record_range_start_sequence_number: bytes = None
-    record_range_end_sequence_number: bytes = None
-    
-    def __init__(self, data: bytes = b"", pos: int = 0):
-        super().__init__(data, pos)
-        self.packet_id: int = ProtocolInfo.NACK
-        
-    def encode_payload(self):
-        self.record_count
-        self.record_single_sequence_number
-        self.record_start_sequence_number
-        self.record_range_start_sequence_number
-        self.record_range_end_sequence_number
+
+class Nack(Acknowledgement):
+    packet_id = ProtocolInfo.NACK
