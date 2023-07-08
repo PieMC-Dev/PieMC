5ccfd909cef6b369388be7282b53196bf385fd78
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2023-07-08 20:03:18 +0300

    Create gr.py

---
+++ lang/gr.py
@@ -0,0 +1,30 @@
+#
+#
+# //--------\\    [----------]   ||--------]   ||\      /||    ||----------]
+# ||        ||         ||        ||            ||\\    //||    ||
+# ||        //         ||        ||======|     || \\  // ||    ||
+# ||-------//          ||        ||            ||  \\//  ||    ||
+# ||                   ||        ||            ||   —–   ||    ||
+# ||              [----------]   ||--------]   ||        ||    ||----------]
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# @author PieMC Team
+# @link http://www.PieMC-Dev.github.io/
+#
+#
+#
+
+RUNNING = "Ο διακομιστής άνοιξε"
+STOP = "Ο διακομιστής έκλεισε"
+NEWPACKET = "Εισερχόμενο GamePacket από"
+DISCONNECTED = "Αποσυνδέθηκε."
+CONNECTING = "Γίνεται σύνδεση..."
+DISCONNECT_PACKET = "Το PieMC δεν είναι έτοιμο.\nΔουλεύουμε πάνω του πρωί και βράδυ.\nΑν θές να μας βηθήσεις, μπορείς να συμβάλλεις στο:\nhttps://github.com/PieMC-Dev/PieMC"
+IP = "IP διακομιστή"
+PORT = "Θύρα"
+GAMEMODE = "Τρόπος παιχνιδιού"
+MAX_PLAYERS = "Μέγιστοι παίχτες"
+CREATED_PIEUID = "Προστέθηκε το pieuid.dat και το UID του διακομιστή"
