16313f12cef12d79784292ddbee2f27a9c86bca0
andiricum2 <andiricum2@gmail.com>
2023-07-19 00:07:37 +0200

    Convert lang file format to yaml

--- piemc/handlers/lang.py
+++ piemc/handlers/lang.py
@@ -13,11 +13,10 @@
 # @author PieMC Team
 # @link http://www.PieMC-Dev.github.io/
 
-import json
+import yaml
 import os
 
 from pathlib import Path
-
 from piemc import config
 
 
@@ -27,19 +26,19 @@ class LangHandler:
         current_dir = Path(__file__).resolve().parent.parent
         lang_dirname = "lang"
         lang_fullpath = os.path.join(current_dir, lang_dirname)
-        lang_file_path = os.path.join(lang_fullpath, f"{config.LANG}.json")
-        fallback_lang_file_path = os.path.join(lang_fullpath, "en.json")
+        lang_file_path = os.path.join(lang_fullpath, f"{config.LANG}.yml")
+        fallback_lang_file_path = os.path.join(lang_fullpath, "en.yml")
 
         if not os.path.exists(lang_file_path):
             print(f"Language file not found for language: {config.LANG}")
             lang = {}
         else:
             with open(lang_file_path, 'r', encoding='utf-8') as lang_file:
-                lang = json.load(lang_file)
+                lang = yaml.safe_load(lang_file)
 
         if os.path.exists(fallback_lang_file_path):
             with open(fallback_lang_file_path, 'r', encoding='utf-8') as fallback_lang_file:
-                fallback_lang = json.load(fallback_lang_file)
+                fallback_lang = yaml.safe_load(fallback_lang_file)
                 lang = {**fallback_lang, **lang}
 
         return lang
