1d82a7a7176f6f8bcad15260856712b8a4f676a2
LapisMYT <nikitagavrilin005@gmail.com>
2023-09-05 12:28:48 +0300

    Upgraded server.py

--- piemc/server.py
+++ piemc/server.py
@@ -26,56 +26,68 @@ from piemc.meta.protocol_info import ProtocolInfo
 import piemc.commands
 from piemc.update import check_for_updates
 
-from pieraknet import Server
-from pieraknet.packets.game_packet import GamePacket
-from pieraknet.packets.frame_set import Frame
-from pieraknet.connection import Connection
+from piebedrock import BedrockServer
 
 
-class MCBEServer:
-    def __init__(self, hostname, port):
+class PieMC_Server:
+    def __init__(self,
+                 hostname="0.0.0.0",
+                 port=19132,
+                 motd1="PieMC Server",
+                 motd2="Powered by PieMC",
+                 version_name="1.20.12",
+                 protocol_version=594,
+                 max_players=20,
+                 gamemode="survival",
+                 guid=random.randint(1, 99999999),
+                 raknet_version=11,
+                 timeout=20
+                 ):
         self.threads = []
         self.lang = LangHandler.initialize_language()
         self.logger = create_logger('PieMC')
         self.logger.info(self.lang['INITIALIZING'])
+
         if not os.path.exists("pieuid.dat"):
             pieuid = random.randint(10 ** 19, (10 ** 20) - 1)
             with open("pieuid.dat", "w") as uid_file:
                 uid_file.write(str(pieuid))
             self.logger.info(f"{self.lang['CREATED_PIEUID']}: {str(pieuid)}")
-        self.server_status = None
-        self.hostname = hostname
-        self.edition = "MCPE"
-        self.protocol_version = 594
-        self.version_name = "1.20.12"
-        self.motd = config.MOTD
-        self.level = "Powered by PieMC"
-        self.players_online = 2  # 2 players online XD. Update (By andiri): YES :sunglasses:
-        self.max_players = config.MAX_PLAYERS
+        with open('pieuid.dat', 'r') as f:
+            pieuid = f.read().strip()
+
         self.gamemode_map = {
             "survival": ("Survival", 1),
             "creative": ("Creative", 2),
             "adventure": ("Adventure", 3)
         }
-        self.gamemode = self.gamemode_map.get(config.GAMEMODE.lower(), ("Survival", 0))
-        self.logger.info(self.lang['NOT_EXISTING_GAMEMODE']) if self.gamemode[1] == 0 else None
-        self.port = config.PORT
+        gamemode = self.gamemode_map.get(gamemode, ("Survival", 0))
+        self.logger.info(self.lang['NOT_EXISTING_GAMEMODE']) if gamemode[1] == 0 else None
+
+        self.port = port
         self.port_v6 = 19133
         self.guid = random.randint(1, 99999999)
-        with open('pieuid.dat', 'r') as f:
-            pieuid = f.read().strip()
-        self.uid = pieuid
         self.raknet_version = 11
         self.timeout = 20
-        self.raknet_server = Server(self.hostname, self.port, create_logger('PieRakNet'))
-        self.raknet_server.interface = self
-        self.update_server_status()
-        self.raknet_server.protocol_version = self.raknet_version
-        self.raknet_server.timeout = self.timeout
-        # self.raknet_server.magic = ''
-        self.raknet_thread = threading.Thread(target=self.raknet_server.start)
-        self.raknet_thread.daemon = True
-        self.threads.append(self.raknet_thread)
+        self.uid = pieuid
+        self.gamemode = gamemode
+
+        self.bedrock_server = BedrockServer(self.hostname, self.port, create_logger("PieBedrock"), self.gamemode, self.timeout)
+        self.bedrock_server.protocol_version = protocol_version
+        self.bedrock_server.version_name = version_name
+        self.bedrock_server.motd1 = motd1
+        self.bedrock_server.motd2 = motd2
+        self.bedrock_server.max_players = max_players
+        self.bedrock_server.uid = uid
+        self.bedrock_server.guid = guid
+        self.bedrock_server.raknet_version = raknet_version
+        self.bedrock_server.raknet_init()
+        self.raknet_server = self.bedrock_server.raknet_server
+
+        self.network_thread = threading.Thread(target=self.bedrock_server.start)
+        self.network_thread.daemon = True
+        self.threads.append(self.network_thread)
+
         self.running = False
         self.cmd_handler = handle_command
         self.logger.info(self.lang['SERVER_INITIALIZED'])
@@ -85,40 +97,9 @@ class MCBEServer:
     def get_time_ms(self):
         return round(time.time() - self.start_time, 4)
 
-    def update_server_status(self):
-        self.server_status = ";".join([
-            self.edition,
-            self.motd,
-            f"{self.protocol_version}",
-            self.version_name,
-            f"{self.players_online}",
-            f"{self.max_players}",
-            f"{self.uid}",
-            self.level,
-            self.gamemode[0],
-            f"{self.gamemode[1]}",
-            f"{self.port}",
-            f"{self.port_v6}"
-        ]) + ";"
-        self.raknet_server.name = self.server_status
-
-    def on_game_packet(self, packet: GamePacket, connection: Connection):
-        packet.decode()
-        if packet.body[0] == ProtocolInfo.LOGIN:
-            self.logger.info(f"New Login Packet: {str(packet.body)}")
-
-    def on_new_incoming_connection(self, connection: Connection):
-        self.logger.info(f"New Incoming Connection: {str(connection.address)}")
-
-    def on_disconnect(self, connection: Connection):
-        self.logger.info(f"{str(connection.address)} disconnected")
-
-    def on_unknown_packet(self, packet: Frame, connection: Connection):
-        self.logger.info(f"New Unknown Packet: {str(packet.body)}")
-
     def start(self):
         self.running = True
-        self.raknet_thread.start()
+        self.network_thread.start()
         self.logger.info(f"{self.lang['RUNNING']} ({self.get_time_ms()}s.)")
         self.logger.info(f"{self.lang['IP']}: {self.hostname}")
         self.logger.info(f"{self.lang['PORT']}: {self.port}")
@@ -137,11 +118,11 @@ class MCBEServer:
     def stop(self):
         self.logger.info(self.lang['STOPPING_WAIT'])
         self.running = False
-        self.raknet_server.stop()
-        self.raknet_thread.join()
+        self.network_server.stop()
+        self.network_thread.join()
         self.logger.info(self.lang['STOP'])
 
 
 if __name__ == "__main__":
-    server = MCBEServer(config.HOST, config.PORT)
+    server = PieMC_Server(config.HOST, config.PORT)
     server.start()
