14d78bb390f4a065f765692bc02faf009053d27f
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2024-05-31 13:46:33 -0500

    Simplify start.py

--- start.py
+++ start.py
@@ -13,21 +13,7 @@
 # @author PieMC Team
 # @link http://www.PieMC-Dev.github.io/
 
-import subprocess
-def check_python_version():
-    try:
-        # Attempt to run the 'python' command and check if it's available
-        subprocess.check_output('python --version', shell=True)
-        return 'python'
-    except subprocess.CalledProcessError:
-        try:
-            # Attempt to run the 'python3' command and check if it's available
-            subprocess.check_output('python3 --version', shell=True)
-            return 'python3'
-        except subprocess.CalledProcessError:
-            raise Exception("Neither 'python' nor 'python3' found in your system.")
+from piemc import main
 
-
-selected_python_command = check_python_version()
-print(f"launching with the command: {selected_python_command}")
-subprocess.call(['python', '-m', 'piemc'])
+if __name__ == "__main__":
+    main()
